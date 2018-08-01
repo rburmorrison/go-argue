ec68f7dece3e243864877dda214a2b8f05e265d9
rburmorrison <r_burmeister@outlook.com>
2018-08-01 16:32:04 -0400

    Add PrintVersion function to Argument

--- pkg/argue/argue.go
+++ pkg/argue/argue.go
@@ -1,3 +1,9 @@
 // Package argue implements functions and types that
 // assist in parsing command-line arguments.
 package argue
+
+import "os"
+
+func getBinaryName() string {
+	return os.Args[0][2:]
+}
--- pkg/argue/argument.go
+++ pkg/argue/argument.go
@@ -1,5 +1,9 @@
 package argue
 
+import (
+	"fmt"
+)
+
 // Argument represents the rules and information that
 // argue must follow when parsing command-line
 // arguments.
@@ -12,9 +16,10 @@ type Argument struct {
 // NewArgument accepts a description and will return
 // a new Argument with that description and default
 // values.
-func NewArgument(desc string) Argument {
+func NewArgument(desc string, version string) Argument {
 	var agmt Argument
 	agmt.Description = desc
+	agmt.Version = version
 
 	return agmt
 }
@@ -22,15 +27,15 @@ func NewArgument(desc string) Argument {
 // AddBool adds a new bool fact to the argument with
 // the given parameters.
 func (agmt *Argument) AddBool(name string, help string) *Fact {
-	var boolFact *Fact
+	var boolFact Fact
 	boolFact.Type = FactTypeBool
 	boolFact.FullName = name
 	boolFact.ShortName = name[0]
 	boolFact.Positional = false
 	boolFact.Required = false
 
-	agmt.Facts = append(agmt.Facts, boolFact)
-	return boolFact
+	agmt.Facts = append(agmt.Facts, &boolFact)
+	return &boolFact
 }
 
 // Propose accepts a boolean that determines if the
@@ -42,4 +47,17 @@ func (agmt Argument) Propose(ms bool) bool {
 // PrintUsage writes the usage information of the
 // recieved argument to the standard output.
 func (agmt Argument) PrintUsage() {
+	agmt.PrintVersion()
+	fmt.Printf("Usage: %v [--example-arg]\n", getBinaryName())
+	fmt.Println()
+	fmt.Println("Positional arguments:")
+	fmt.Println()
+	fmt.Println("Flags:")
+}
+
+// PrintVersion writes the version of the program
+// to the standard output in the form of "<name>
+// <version>"
+func (agmt Argument) PrintVersion() {
+	fmt.Printf("%v %v\n", getBinaryName(), agmt.Version)
 }
